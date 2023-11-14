8fc476833599333a6ee458d0f5a9f6ce1097168d
jinhyeon <tiemo0708@naver.com>
2023-11-15 01:44:04 +0900

    feat: Application과 view 연결

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView output = new OutputView();
+
     }
 }
