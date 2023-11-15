2b2f89f8c3ea5f5b370090c02838fc87e3806de5
jinhyeon <tiemo0708@naver.com>
2023-11-15 21:50:12 +0900

    chore: 이벤트 이름 변경

--- src/main/java/christmas/model/Events.java
+++ src/main/java/christmas/model/Events.java
@@ -1,11 +1,11 @@
 package christmas.model;
 
 public enum Events {
-    CHRISTMAS_D_DAY_DISCOUNT("totalOrderDiscount",1000),
-    WEEKDAY_DISCOUNT("dessertDiscount",2023),
-    WEEKEND_DISCOUNT("mainDiscount",2023),
-    SPECIAL_DISCOUNT("TotalOrderDiscount",1000),
-    PRESENT("gift",25000),
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인",1000),
+    WEEKDAY_DISCOUNT("평일 할인",2023),
+    WEEKEND_DISCOUNT("주말 할인",2023),
+    SPECIAL_DISCOUNT("특별 할인",1000),
+    PRESENT("증정 이벤트",25000),
     NO_EVENTS("",0);
     private final String discountKind;
     private int amount;
