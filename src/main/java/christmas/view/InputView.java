0115504a47680db2c8001acc3eb590b35b3e7b6a
jinhyeon <tiemo0708@naver.com>
2023-12-05 01:03:35 +0900

    feat: 인풋 출력 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,21 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+    private static final String ASK_DATE = ("%n%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    private static final String ASK_MENU = ("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    private static final String MONTH =("12");
+
+    public String readDate() {
+        System.out.printf(ASK_DATE, MONTH);
+        return Console.readLine();
+    }
+    public List<String> readMenu(){
+        System.out.println(ASK_MENU);
+        return Arrays.asList(Console.readLine().split(","));
+    }
 }
--- src/main/java/christmas/view/OutputVIew.java
+++ src/main/java/christmas/view/OutputVIew.java
@@ -1,4 +1,5 @@
 package christmas.view;
 
 public class OutputVIew {
+    private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.";
 }
