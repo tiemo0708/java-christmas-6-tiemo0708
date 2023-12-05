50c0eb74ab82dafeaf5ac20dca90297e1bdfb45f
jinhyeon <tiemo0708@naver.com>
2023-12-06 05:20:23 +0900

    fix: 에러 클래스 이름 변경 ERROR -> Error

--- src/main/java/christmas/global/Error.java
+++ src/main/java/christmas/global/Error.java
@@ -1,10 +1,10 @@
 package christmas.global;
 
-public enum ERROR {
+public enum Error {
     INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     private static final String ERROR = "[ERROR] ";
     private final String message;
-    ERROR(String message) {
+    Error(String message) {
 
         this.message = ERROR+message;
     }
